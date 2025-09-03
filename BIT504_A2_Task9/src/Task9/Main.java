d61e0f4fa5ee30f144bc2b3eca1768d545cbd829
Aaron Witten <a.witten94@gmail.com>
2025-09-03 12:31:21 +1200

    Added 2 new Print statements to the Main class.

--- BIT504_A2_Task9/src/Task9/Main.java
+++ BIT504_A2_Task9/src/Task9/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is to test GitHub Source control");
 		System.out.println("This is to is the first change to test GitHub Source control");
+		System.out.println("This is to is the Second change to test GitHub Source control");
+		System.out.println("This is to is the Another change to test GitHub Source control");
 		
 	}
 
